95cc91ff68f70b761664c70304d5fc2a21cb0d2c
Vikas Maurya <vikaskumarmaurya765@gmail.com>
2023-10-09 11:22:43 +0530

    third commit

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,42 +1,48 @@
 import React from 'react';
 import './navBar.css';
+import { IoMdMenu } from 'react-icons/io';
 
 const NavBar = () => {
-    return <div className='nav_bar'>
-        <div className='tabs'>
-       <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Banglor</option></select></p>
+  return <div className='nav_bar'>
+    <div className='menu_bar'>
+      <IoMdMenu/>
+
+    </div>
+    <div className='tabs'>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Banglor</option></select></p>
+
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Chennai</option></select></p>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Delhi</option></select></p>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Gurgaon</option></select></p>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Hyderabad</option></select></p>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Kolkata</option></select></p>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Mumbai</option></select></p>
+      <p>
+        <select className='toggle' name="cars" id="cars">
+          <option value="volvo">Noida</option></select></p>
+    </div>
+    <div className='nav_search'>
+
+      <input placeholder='search cities'></input><span><img src='./Assest/Main-Search-Icon.png'></img></span>
+    </div>
     
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Chennai</option></select></p>
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Delhi</option></select></p>
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Gurgaon</option></select></p>
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Hyderabad</option></select></p>
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Kolkata</option></select></p>
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Mumbai</option></select></p>
-            <p>
-  <select className='toggle' name="cars" id="cars">
-    <option value="volvo">Noida</option></select></p>
-        </div>
-        <div className='nav_search'>
-
-            <input placeholder='search cities'></input><span><img src='./Assest/Main-Search-Icon.png'></img></span>
-        </div>
 
 
-    </div>;
+  </div>;
 }
 
 
